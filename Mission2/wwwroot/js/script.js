a957eef1062f3726d888bcb470d7229a0aa86be9
mattcperry382 <mattcperry382@gmail.com>
2022-01-12 18:53:05 -0700

    Add project files.

---
+++ Mission2/wwwroot/js/script.js
@@ -0,0 +1,23 @@
+﻿$('#calculate').click((event) => {
+    assignments = $('#assignments').val() * 55 / 100;
+    groupProject = $('#group-project').val() * 5 / 100;
+    quizzes = $('#quizzes').val() * 10 / 100;
+    exams = $('#exams').val() * 20 / 100;
+    intex = $('#intex').val() * 10 / 100;
+    finalGrade = assignments + groupProject + quizzes + exams + intex;
+
+    if (finalGrade >= 94) letterGrade = 'A'
+    else if (finalGrade >= 90) letterGrade = 'A-'
+    else if (finalGrade >= 87) letterGrade = 'B+'
+    else if (finalGrade >= 84) letterGrade = 'B'
+    else if (finalGrade >= 80) letterGrade = 'B-'
+    else if (finalGrade >= 77) letterGrade = 'C+'
+    else if (finalGrade >= 74) letterGrade = 'C'
+    else if (finalGrade >= 70) letterGrade = 'C-'
+    else if (finalGrade >= 67) letterGrade = 'D+'
+    else if (finalGrade >= 64) letterGrade = 'D'
+    else if (finalGrade >= 60) letterGrade = 'D-'
+    else letterGrade = 'E';
+
+    alert(`Your final grade in IS 413 will be ${letterGrade} at ${finalGrade}%.`);
+})
