b82d2cf58e1b794826ccfe890b84d59118311b92
mattcperry382 <mattcperry382@gmail.com>
2022-01-12 19:51:18 -0700

    mission 2 commit

--- IS 413/wwwroot/js/script.js
+++ IS 413/wwwroot/js/script.js
@@ -1,11 +1,14 @@
 ﻿$('#calculate').click((event) => {
+    // calculate each as if that's the grade percentage for just that assignment type
     assignments = $('#assignments').val() * 55 / 100;
     groupProject = $('#group-project').val() * 5 / 100;
     quizzes = $('#quizzes').val() * 10 / 100;
     exams = $('#exams').val() * 20 / 100;
     intex = $('#intex').val() * 10 / 100;
+    // add together
     finalGrade = assignments + groupProject + quizzes + exams + intex;
 
+    // calculate the letter grade based off the percentage
     if (finalGrade >= 94) letterGrade = 'A'
     else if (finalGrade >= 90) letterGrade = 'A-'
     else if (finalGrade >= 87) letterGrade = 'B+'
@@ -19,5 +22,6 @@
     else if (finalGrade >= 60) letterGrade = 'D-'
     else letterGrade = 'E';
 
+    // display results
     alert(`Your final grade in IS 413 will be ${letterGrade} at ${finalGrade}%.`);
 })
